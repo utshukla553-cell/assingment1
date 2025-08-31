ed8bb272756c742a24788fa4448d23830d863996
Uttam Shukla <utshukla553@gamil.com>
2025-08-31 09:22:45 +0530

    assigment completed

---
+++ Task1.py
@@ -0,0 +1,14 @@
+num1 = int(input("enter the first number: "))
+num2 = int(input("enter the second number: "))
+print("the numebrs you entered are:")
+
+# Perform mathematical operations
+addition= num1 + num2
+substruction= num1 - num2
+multiplication= num1 * num2
+division= num1 / num2 
+print("Addition: "+str(addition))
+print("Substruction :"+str(substruction))
+print("Multiplication: "+str(multiplication))
+print("Division:"+str(division))
+
---
+++ Task2.py
@@ -0,0 +1,4 @@
+first_name = input("enter the first name:")
+last_name = input("enter the last name:")
+full_name = first_name + " " + last_name
+print(f"hello,{full_name}!welcome shukla program")
